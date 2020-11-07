3156b9086ff1894fb579b4afdc95f22ce4a6fc09
Peter Truman <peter.truman@gmail.com>
2020-11-07 13:18:19 +0000

    Update diab_startup.sh
    Added check for variable before testing variable (caused a unary operator error before).

--- scripts/diab_startup.sh
+++ scripts/diab_startup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-if [ $DIAB_ENABLE_ADVANCED_LOGGING -eq 1 ]; then
-        advlog="-v"
-else
-        advlog=""
+if [ $DIAB_ENABLE_ADVANCED_LOGGING ]; then
+        if [ $DIAB_ENABLE_ADVANCED_LOGGING -eq 1 ]; then
+                advlog="-v"
+        else
+                advlog=""
+        fi
 fi
 /usr/sbin/diab_confbuild.sh
 dnsdist -C /etc/dnsdist/dnsdist.conf --supervised $advlog
