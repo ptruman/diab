2780d17b3855b61430d8d2d5513528729495f702
Peter Truman <peter.truman@gmail.com>
2020-12-23 14:33:24 +0000

    Update diab_startup.sh
    Added comments and included routedns in startup

--- scripts/diab_startup.sh
+++ scripts/diab_startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# DIAB Startup Script
+# Set Version
+#
+# Check for/enable advanced logging
 if [ $DIAB_ENABLE_ADVANCED_LOGGING ]; then
         if [ $DIAB_ENABLE_ADVANCED_LOGGING -eq 1 ]; then
                 advlog="-v"
@@ -6,5 +10,9 @@ if [ $DIAB_ENABLE_ADVANCED_LOGGING ]; then
                 advlog=""
         fi
 fi
+# Run the configurator....
 /usr/sbin/diab_confbuild.sh
-dnsdist -C /etc/dnsdist/dnsdist.conf --supervised $advlog
+# Start dnsdist in the background
+dnsdist -C /etc/dnsdist/dnsdist.conf --supervised $advlog &
+# Start routedns
+routedns /etc/routedns/*.toml
