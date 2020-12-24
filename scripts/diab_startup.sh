5817d345473c74c351dd6fb86ce6005ce30cedd2
Peter Truman <peter.truman@gmail.com>
2020-12-24 13:04:26 +0000

    Update diab_startup.sh

--- scripts/diab_startup.sh
+++ scripts/diab_startup.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 # DIAB Startup Script
-# Set Version
 #
+echo "#"
+echo "# DIAB : INFO    : Attempting to start diab..."
+echo "#"
+# Run the configurator....
+echo "# DIAB : INFO    : Launching diab configurator..."
+/usr/sbin/diab_confbuild.sh
 # Check for/enable advanced logging
 if [ $DIAB_ENABLE_ADVANCED_LOGGING ]; then
         if [ $DIAB_ENABLE_ADVANCED_LOGGING -eq 1 ]; then
+                echo "# DIAB : INFO    : Advanced logging requested..."
                 advlog="-v"
         else
                 advlog=""
         fi
 fi
-# Run the configurator....
-/usr/sbin/diab_confbuild.sh
 # Check if we should start routedns, or just dnsdist...
 if [ `ls /etc/routedns | wc -l` -eq 0 ]; then
         # No files - start dnsdist only...
+        echo "# DIAB : INFO    : Launching dnsdist..."
         dnsdist -C /etc/dnsdist/dnsdist.conf --supervised $advlog
 else
         # Files found - start dnsdist (background) and routedns
+        echo "# DIAB : INFO    : Launching dnsdist and routedns..."
         dnsdist -C /etc/dnsdist/dnsdist.conf --supervised $advlog &
         routedns /etc/routedns/*.toml
 fi
-
