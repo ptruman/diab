dc28a2278955941929befa4fccb937d58f57ebb9
Peter Truman <peter.truman@gmail.com>
2020-12-24 12:57:43 +0000

    Update diab_startup.sh
    Fix for missing routedns files on startup

--- scripts/diab_startup.sh
+++ scripts/diab_startup.sh
@@ -12,7 +12,13 @@ if [ $DIAB_ENABLE_ADVANCED_LOGGING ]; then
 fi
 # Run the configurator....
 /usr/sbin/diab_confbuild.sh
-# Start dnsdist in the background
-dnsdist -C /etc/dnsdist/dnsdist.conf --supervised $advlog &
-# Start routedns
-routedns /etc/routedns/*.toml
+# Check if we should start routedns, or just dnsdist...
+if [ `ls /etc/routedns | wc -l` -eq 0 ]; then 
+        # No files - start dnsdist only...
+        dnsdist -C /etc/dnsdist/dnsdist.conf --supervised $advlog
+else    
+        # Files found - start dnsdist (background) and routedns
+        dnsdist -C /etc/dnsdist/dnsdist.conf --supervised $advlog &
+        routedns /etc/routedns/*.toml
+fi
+
